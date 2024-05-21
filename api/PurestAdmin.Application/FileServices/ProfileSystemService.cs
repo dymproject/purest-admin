637575ffc8d3cc84180eea3d3f5088791c4c9fdf
dymproject <bolixinde@qq.com>
2024-05-21 18:07:37 +0800

    更新依赖包到最新版，新增系统文件上传下载接口，ProfileSystem

---
+++ api/PurestAdmin.Application/FileServices/ProfileSystemService.cs
@@ -0,0 +1,70 @@
+﻿// Copyright © 2023-present https://github.com/dymproject/purest-admin作者以及贡献者
+
+using System.Web;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+using PurestAdmin.Core.File;
+
+using Volo.Abp.BlobStoring;
+
+using Yitter.IdGenerator;
+
+namespace PurestAdmin.Application.FileServices;
+
+public class ProfileSystemService(IConfiguration configuration, ISqlSugarClient db, IBlobContainer<ProfileSystemContainer> blobContainer) : ApplicationService
+{
+    private readonly ISqlSugarClient _db = db;
+    private readonly IBlobContainer<ProfileSystemContainer> _blobContainer = blobContainer;
+    private readonly IConfiguration _configuration = configuration;
+    /// <summary>
+    /// 单文件上传
+    /// </summary>
+    /// <param name="file"></param>
+    /// <returns></returns>
+    public async Task<long> UploadAsync([Required(ErrorMessage = "文件不能为空！")] IFormFile file)
+    {
+        if (file.Length == 0)
+        {
+            throw Oops.Bah("检测到空文件！");
+        }
+        //限制文件上传大小默认100M
+        var limitSize = _configuration["FileStorageOptions:LimitSize"] ?? "100";
+        if (file.Length / 1024 > int.Parse(limitSize) * 1024)
+        {
+            throw Oops.Bah($"上传的文件不能超过{limitSize}");
+        }
+        var id = YitIdHelper.NextId();
+        var ext = Path.GetExtension(file.FileName);
+        var size = (int)file.Length / 1024 + 1;
+
+        var fileRecord = new FileRecordEntity()
+        {
+            Id = id,
+            FileExt = ext,
+            FileName = file.FileName.Trim(),
+            FileSize = size
+        };
+        await _blobContainer.SaveAsync(id.ToString(), file.GetAllBytes(), true);
+        await _db.Insertable(fileRecord).ExecuteCommandAsync();
+        return id;
+    }
+
+    /// <summary>
+    /// 文件下载
+    /// </summary>
+    /// <param name="fileId"></param>
+    /// <returns>FileStreamResult</returns>
+    [HttpGet]
+    public async Task<IActionResult> DownloadAsync([Required(ErrorMessage = "请输入文件id"), Range(1, long.MaxValue)] long fileId)
+    {
+        var fileRecord = await _db.Queryable<FileRecordEntity>().FirstAsync(x => x.Id == fileId);
+        var bytes = await _blobContainer.GetAllBytesAsync(fileId.ToString());
+        return new FileContentResult(bytes, "application/octet-stream")
+        {
+            FileDownloadName = HttpUtility.UrlEncode(fileRecord.FileName)
+        };
+    }
+}
---
+++ api/PurestAdmin.Core/File/ProfileSystemContainer.cs
@@ -0,0 +1,9 @@
+﻿// Copyright © 2023-present https://github.com/dymproject/purest-admin作者以及贡献者
+
+using Volo.Abp.BlobStoring;
+
+namespace PurestAdmin.Core.File;
+[BlobContainerName("profile-system")]
+public class ProfileSystemContainer
+{
+}
