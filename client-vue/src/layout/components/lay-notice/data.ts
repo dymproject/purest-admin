a63d039aa165ee84f313c9460a74f3c6679c511b
dymproject <bolixinde@qq.com>
2024-05-27 15:48:08 +0800

    同步上游版本

---
+++ client-vue/src/layout/components/lay-notice/data.ts
@@ -0,0 +1,16 @@
+export interface ListItem {
+  avatar: string;
+  title: string;
+  dateTime: string;
+  type: string;
+  description: string;
+  status?: "primary" | "success" | "warning" | "info" | "danger";
+  extra?: string;
+}
+
+export interface TabItem {
+  key: string;
+  name: string;
+  list: ListItem[];
+  emptyText: string;
+}
