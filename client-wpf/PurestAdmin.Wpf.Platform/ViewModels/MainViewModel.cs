a9c29ac7d52d57e65c2c91c2426acb8c6ee353c8
dym <bolixinde@qq.com>
2025-09-14 21:06:42 +0800

    实现菜单模糊搜索

--- client-wpf/PurestAdmin.Wpf.Platform/ViewModels/MainViewModel.cs
+++ client-wpf/PurestAdmin.Wpf.Platform/ViewModels/MainViewModel.cs
@@ -12,18 +12,21 @@ namespace PurestAdmin.Wpf.Platform.ViewModels
     {
         public MainViewModel()
         {
-            Items =
-            [
-                new PuaMenuItem("用户管理",new User(),["system.user"],IconType.UserLine),
-                new PuaMenuItem("角色管理",new Role(),["system.role"],IconType.TeamLine),
-                new PuaMenuItem("功能管理",new Function(),["system.function"],IconType.FunctionLine),
-                new PuaMenuItem("组织机构",new Organization(),["system.organization"],IconType.OrganizationChart),
-                new PuaMenuItem("字典管理",new Dictionary(),["system.dictionary"],IconType.StickyNoteLine),
-                new PuaMenuItem("配置管理",new SystemConfig(),["system.systemconfig"],IconType.SettingsLine),
-                new PuaMenuItem("通知公告",new Notice(),["system.notice"],IconType.UserVoiceLine),
-                new PuaMenuItem("在线用户",new OnlineUser(),["system.onlineuser"],IconType.ContractLine),
-                new PuaMenuItem("请求日志",new RequestLog(),["system.requestlog"],IconType.EvernoteLine),
-            ];
+            Items = new ObservableCollection<PuaMenuItem>
+            {
+                new PuaMenuItem("用户管理", new User(), new[] { "system.user" }, IconType.UserLine),
+                new PuaMenuItem("角色管理", new Role(), new[] { "system.role" }, IconType.TeamLine),
+                new PuaMenuItem("功能管理", new Function(), new[] { "system.function" }, IconType.FunctionLine),
+                new PuaMenuItem("组织机构", new Organization(), new[] { "system.organization" }, IconType.OrganizationChart),
+                new PuaMenuItem("字典管理", new Dictionary(), new[] { "system.dictionary" }, IconType.StickyNoteLine),
+                new PuaMenuItem("配置管理", new SystemConfig(), new[] { "system.systemconfig" }, IconType.SettingsLine),
+                new PuaMenuItem("通知公告", new Notice(), new[] { "system.notice" }, IconType.UserVoiceLine),
+                new PuaMenuItem("在线用户", new OnlineUser(), new[] { "system.onlineuser" }, IconType.ContractLine),
+                new PuaMenuItem("请求日志", new RequestLog(), new[] { "system.requestlog" }, IconType.EvernoteLine),
+            };
+
+            FilteredItems = new ObservableCollection<PuaMenuItem>(); // 先初始化为空
+            FilterItems(); // 调用一次初始化
             CurrentViewItem = Items.First();
         }
 
@@ -32,66 +35,79 @@ namespace PurestAdmin.Wpf.Platform.ViewModels
         /// </summary>
         public ObservableCollection<PuaMenuItem> Items { get; set; }
 
-        private int _selectIndex;
-        /// <summary>
-        /// 选中项
-        /// </summary>
-        public int SelectIndex
-        {
-            get { return _selectIndex; }
-            set { SetProperty(ref _selectIndex, value); }
-        }
-
         private PuaMenuItem _currentViewItem;
         /// <summary>
-        /// 当前选中
+        /// 当前选中项
         /// </summary>
         public PuaMenuItem CurrentViewItem
         {
             get { return _currentViewItem; }
-            set { _currentViewItem = value; }
+            set { SetProperty(ref _currentViewItem, value); }
         }
 
         private string _searchContent;
         /// <summary>
-        /// 菜单搜索
+        /// 菜单搜索内容
         /// </summary>
         public string SearchContent
         {
             get { return _searchContent; }
-            set { _searchContent = value; }
+            set
+            {
+                SetProperty(ref _searchContent, value);
+                FilterItems(); // 调用过滤方法
+            }
+        }
+
+        private ObservableCollection<PuaMenuItem> _filteredItems;
+        /// <summary>
+        /// 过滤后的子项集合
+        /// </summary>
+        public ObservableCollection<PuaMenuItem> FilteredItems
+        {
+            get { return _filteredItems; }
+            set { SetProperty(ref _filteredItems, value); }
+        }
+
+        private void FilterItems()
+        {
+            var filtered = string.IsNullOrWhiteSpace(SearchContent) ? Items : Items.Where(item => item.Name.Contains(SearchContent.Trim(), StringComparison.CurrentCultureIgnoreCase));
+
+            FilteredItems.Clear();
+            foreach (var item in filtered)
+            {
+                FilteredItems.Add(item);
+            }
         }
 
         private bool _menuStatus = false;
-
         public bool MenuStatus
         {
             get { return _menuStatus; }
             set { SetProperty(ref _menuStatus, value); }
         }
 
-
         private DelegateCommand<object> _expandedCommand;
         public DelegateCommand<object> ExpandedCommand => _expandedCommand ??= new DelegateCommand<object>(ExecuteExpandedCommand);
-
         void ExecuteExpandedCommand(object parameter)
         {
-            var e = parameter as RoutedEventArgs;
-            if (e == null)
+            if (parameter is not RoutedEventArgs e)
             {
                 MenuStatus = !MenuStatus;
             }
         }
 
-
         private DelegateCommand<object> _selectCommand;
         public DelegateCommand<object> SelectCommand => _selectCommand ??= new DelegateCommand<object>(ExecuteSelectCommand);
-
         void ExecuteSelectCommand(object view)
         {
             var type = view.GetType();
-            var item = Items.First(x => x.View.GetType() == type);
-            item.View = Activator.CreateInstance(type);
+            var item = Items.FirstOrDefault(x => x.View.GetType() == type);
+            if (item != null)
+            {
+                item.View = Activator.CreateInstance(type);
+                CurrentViewItem = item;
+            }
         }
     }
 }
