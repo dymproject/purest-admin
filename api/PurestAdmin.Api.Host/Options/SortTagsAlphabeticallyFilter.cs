b84a06c912754863b4c893c149c12aeafa54f779
dymproject <bolixinde@qq.com>
2026-05-09 10:26:55 +0800

    让swagger接口按升序排序

---
+++ api/PurestAdmin.Api.Host/Options/SortTagsAlphabeticallyFilter.cs
@@ -0,0 +1,18 @@
+﻿// Copyright © 2023-present https://github.com/dymproject/purest-admin作者以及贡献者
+
+using Microsoft.OpenApi.Models;
+
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace PurestAdmin.Api.Host.Options;
+
+public class SortTagsAlphabeticallyFilter : IDocumentFilter
+{
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        if (swaggerDoc.Tags != null)
+        {
+            swaggerDoc.Tags = [.. swaggerDoc.Tags.OrderBy(t => t.Name)];
+        }
+    }
+}
