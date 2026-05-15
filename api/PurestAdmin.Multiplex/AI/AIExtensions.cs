03f8c3448c8c53156dbe126830422eb72346afb1
dymproject <bolixinde@qq.com>
2026-05-15 13:28:53 +0800

    未配置 AI 相关配置，不启用 AI 功能

--- api/PurestAdmin.Multiplex/AI/AIExtensions.cs
+++ api/PurestAdmin.Multiplex/AI/AIExtensions.cs
@@ -13,6 +13,11 @@ public static class AIExtensions
     public static IServiceCollection AddAIService(this IServiceCollection services, IConfiguration configuration)
     {
         var aiOptions = configuration.GetSection("AIOptions").Get<AIOptions>() ?? throw new Exception("没有找到AI相关配置");
+        if (string.IsNullOrEmpty(aiOptions.Endpoint) || string.IsNullOrEmpty(aiOptions.ModelName) || string.IsNullOrEmpty(aiOptions.ApiKey))
+        {
+            // 未配置 AI 相关配置，不启用 AI 功能
+            return services;
+        }
         //services.AddAntiforgery();如果使用 Cookie 存储用户信息，必须开启 Antiforgery。并配合[ValidateAntiForgeryToken]使用。
         services.AddKernel()
             .AddOpenAIChatCompletion(modelId: aiOptions.ModelName, apiKey: aiOptions.ApiKey, endpoint: new Uri(aiOptions.Endpoint))
